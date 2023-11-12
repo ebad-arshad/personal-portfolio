17cee0a363d48e16f9fcfb7e5018e786d48dfe8d
M.Ebad Arshad <m.ebadarshad2003@gmail.com>
2023-11-12 18:37:32 +0500

    Update CV.tsx

--- components/CV.tsx
+++ components/CV.tsx
@@ -1,6 +1,6 @@
 const CV = () => {
     return (
-        <a href='https://drive.google.com/file/d/1wnZUKoHyiosW3rgdhkhghQviSS-2a9l0/view?usp=sharing' target="_blank">
+        <a href='https://drive.google.com/file/d/1IhynZX1AeGI9lH_OMt6ztJdy60DuIvUW/view?usp=sharing' target="_blank">
             <button
                 className='fixed bottom-6 z-[9999999999999] right-6 shadow-lg border p-4 bg-black/40 text-white'
             >Download CV</button>
